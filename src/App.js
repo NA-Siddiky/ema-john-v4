62cd3b613d96b376f54098c0d69cac4b500a4271
NA-Siddiky <siddiky.academic@gmail.com>
2021-10-02 11:51:29 +0600

    commit start

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-
     </div>
   );
 }
---
+++ src/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div>
+            <h1>Raihan Bhai</h1>
+            <h2>Testing</h2>
+            <p></p>
+        </div>
+    )
+}
+
+export default Test
